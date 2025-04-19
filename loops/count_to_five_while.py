bf3304a51395393a756f73db8e420e4078e9a06c
Amirprg <amirprg.ir@gmail.com>
2025-04-19 18:21:31 +0330

    Implement basic while loop for counting up to 5

---
+++ loops/count_to_five_while.py
@@ -0,0 +1,13 @@
+# ------------------------------------------------------------
+# Title: Count to Five Using While Loop
+# Description:
+#   This script uses a while loop to print numbers from 1 to 5.
+#   A counter variable is initialized to 1 and incremented in each loop
+#   iteration until the condition becomes false.
+# Author: amirprg-ir
+# ------------------------------------------------------------
+
+i = 1
+while i <= 5:
+    print(i)
+    i+=1
