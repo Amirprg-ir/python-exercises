1d1c3a443a460b55b7bba07c98bfd7f1b771e975
Amirprg <amirprg.ir@gmail.com>
2025-04-19 16:05:23 +0330

    Implement basic for loop example using range to print numbers 1 to 10

---
+++ loops/print_1_to_10.py
@@ -0,0 +1,10 @@
+# ------------------------------------------------------------
+# Title: Print Numbers from 1 to 10 - For Loop
+# Description:
+#   This script uses a for loop and the range() function
+#   to print numbers from 1 to 10, each on a new line.
+# Author: amirprg-ir
+# ------------------------------------------------------------
+
+for i in range(1,11):
+    print(i)
