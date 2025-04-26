1a04f28eae9da50c3bbaeced57c95fe28ab3341d
Amirprg <amirprg.ir@gmail.com>
2025-04-26 18:44:16 +0330

    Demonstrate basic function definitions: print vs return

---
+++ functions/define_simple_function.py
@@ -0,0 +1,19 @@
+# ------------------------------------------------------------
+# Title: Define and Call a Simple Function
+# Description:
+#   This script demonstrates how to define simple functions,
+#   one that prints a message directly, and another that returns a message.
+# Author: amirprg-ir
+# ------------------------------------------------------------
+
+def say_hello():
+    print("Hello! Welcome to the Python World.")
+
+say_hello()
+
+# ----------------------------------------
+
+def say_hi():
+    return "Hi! Welcome to the Python World."
+
+print(say_hi())
