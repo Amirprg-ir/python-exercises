9629ada076f9c1f95fa080ffb7200a9b1e6e0bca
Amirprg <amirprg.ir@gmail.com>
2025-04-26 13:37:55 +0330

    Implement creation of an empty set and adding items using add()

---
+++ data-structures/sets/set_add_items.py
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------
+# Title: Create and Add Items to Set
+# Description:
+#   This script creates an empty set and adds three items
+#   using add(), then prints the final set.
+# Author: amirprg-ir
+# ------------------------------------------------------------
+
+empty_set = set()
+
+empty_set.add("amir")
+empty_set.add(24)
+empty_set.add(206)
+
+print(f"The final set is: {empty_set}")
