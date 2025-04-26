1d483666d2d3e91e982851a5a9ce7e806e0bcf8e
Amirprg <amirprg.ir@gmail.com>
2025-04-26 18:56:42 +0330

    Implement a function to add two numbers and return the result

---
+++ functions/function_with_multiple_parameters.py
@@ -0,0 +1,13 @@
+# ------------------------------------------------------------
+# Title: Function with Multiple Parameters
+# Description:
+#   This script demonstrates defining a function that
+#   accepts two parameters, calculates their sum, and returns the result.
+# Author: amirprg-ir
+# ------------------------------------------------------------
+
+def add_numbers(a, b):
+    result = a + b
+    return result
+
+print(f"The sum of 2 and 6 is: {add_numbers(2, 6)}")
